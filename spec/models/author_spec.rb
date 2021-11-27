aeb23b392fa5d22a46d29ecd65b3fa191be303e7
leandermasopust <leander.masopust@gmail.com>
2021-11-27 20:17:23 +0100

    Failing Test for #5

--- spec/models/author_spec.rb
+++ spec/models/author_spec.rb
@@ -5,4 +5,9 @@ RSpec.describe Author, type: :model do
       expect(author.last_name).to eq("Turing")
       expect(author.homepage).to eq("http://wikipedia.org/Alan_Turing")
     end
+
+    it "should return first name and last name concatenated when name is called" do
+        author = Author.new(first_name:"Alan", last_name:"Turing", homepage:"http://wikipedia.org/Alan_Turing")
+        expect(author.name).to eq("Alan Turing")
+    end
 end    
