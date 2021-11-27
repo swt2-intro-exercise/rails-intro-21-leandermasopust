e6ca4939036118a1502f35635d3edcdc37877d0f
leandermasopust <leander.masopust@gmail.com>
2021-11-27 21:30:38 +0100

    Fixed test for #8

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,12 +1,16 @@
 class AuthorsController < ApplicationController
   def new 
+    @author = Author.new
   end
 
   def create
     @author = Author.new(params.require(:author).permit(:first_name,:last_name,:homepage))
 
-    @author.save
-    redirect_to @author
+    if @author.save
+      redirect_to @author
+    else
+      render 'new'
+    end
   end
 
   def show
