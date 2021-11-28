4ef7305a1fb25e9b733b1ce4e929e61f4072da03
leandermasopust <leander.masopust@gmail.com>
2021-11-28 16:19:52 +0100

    Failing and Fixed test for #13

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -3,4 +3,5 @@ class Paper < ApplicationRecord
     validates :venue, presence: true
     validates :year, presence: true
     validates :year, numericality: { only_integer: true }
+    has_and_belongs_to_many :authors
 end
--- spec/models/paper_spec.rb
+++ spec/models/paper_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Paper, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should have no authors" do
+    paper = Paper.new(title:"Title", venue:"Venue", year:"2021")
+    expect(paper.authors).to be_empty
+  end
 end
